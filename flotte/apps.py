a903849cd7770078c8e406fb0e1f06fb77732756
Yvan2007 <kouayavana20@gmail.com>
2026-02-02 12:05:52 +0000

    docs(apps): docstring FlotteConfig

--- flotte/apps.py
+++ flotte/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 
 class FlotteConfig(AppConfig):
+    """Configuration de l'application FLOTTE."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'flotte'
     verbose_name = 'FLOTTE — Gestion parc véhicules'
